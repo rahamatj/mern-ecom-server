7f938b78413aa36c0b4bbd5a00e1d76f5802eb19
rahamatj <32268831+rahamatj@users.noreply.github.com>
2025-11-15 19:41:28 +0600

    add cart count

--- utils/server.config.js
+++ utils/server.config.js
@@ -11,7 +11,7 @@ function config() {
 
     cfg["prod"] = {
         PORT: 3001,
-        API_URL: "https://72.60.219.158:3001",
+        API_URL: "http://72.60.219.158:3001",
         MONGO_URI: "mongodb+srv://rahamatj:dhRmfxWw5NFHBMDc@ecom.m8h6nnq.mongodb.net/?appName=ecom"
         // MONGO_URI: "mongodb://127.0.0.1:27017/ecom"
     }
