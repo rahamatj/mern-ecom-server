4b2d8247c48cddb0b987f27d15e6f44f74263f47
Rahamat Jahan <rahamatj92@gmail.com>
2026-06-13 20:27:59 +0600

    bug fixes

--- index.js
+++ index.js
@@ -12,16 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const MONGO_URI = process.env.MONGO_URI;
-
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "http://mern-ecom.com",
-    ],
-    credentials: true,
-}));
 
 // Root route
 app.get('/', (req, res) => {
